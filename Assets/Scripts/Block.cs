01880dca58e3159130b572fe40511a265daa91f3
Ege Ozeren <180202047@kocaeli.edu.tr>
2020-06-10 01:19:04 +0300

    starting of the project

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    [SerializeField] AudioClip breakSound;
+    
+    // cached reference
+    Level level;
+    GameSession gameSession;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        gameSession = FindObjectOfType<GameSession>();
+        level.CountBreakableBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        gameSession.AddToScore();
+        Destroy(gameObject);
+        level.BlockDestroyed();
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+   /* [SerializeField] int currentScore = 0, pointPerBlock = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+    
+
+    public void increaseScore()
+    {
+        currentScore += pointPerBlock;
+        scoreText.text = currentScore.ToString();
+    }
+   */
+}
